a3e75c5dd0190af64367261fc0f1d92bceda4f33
SomeKittens <rkoutnik@gmail.com>
2014-05-27 14:34:00 -0700

    lots more tests and more efficient prime tester

--- test/fizz.js
+++ test/fizz.js
@@ -3,10 +3,12 @@
 require('should');
 
 var oneto100 = require('./ans/1-100.json')
+  , oneto100p = require('./ans/1-100p.json')
   , exec = require('child_process').exec;
 
 var standardFizz = require('../lib/standard')
-  , yieldFizz = require('../lib/yield');
+  , yieldFizz = require('../lib/yield')
+  , findPrime = require('../lib/prime');
 
 describe('fizzbuzz', function() {
   var results = []
@@ -21,7 +23,7 @@ describe('fizzbuzz', function() {
 
     describe('standard', function() {
       it('should run through the basic fizzbuzz test', function() {
-        standardFizz(1, 100, addToResults);
+        standardFizz({s: 1, e: 100}, addToResults);
 
         results.length.should.eql(100);
         for (var i = 0; i < 100; i++) {
@@ -29,18 +31,26 @@ describe('fizzbuzz', function() {
         }
       });
       it('works on numbers other than 1-100', function() {
-        standardFizz(1, 10, addToResults);
+        standardFizz({s: 1, e: 10}, addToResults);
 
         results.length.should.eql(10);
         for (var i = 0; i < 10; i++) {
           results[i].should.eql(oneto100[i]);
+        }
+      });
+      it('can also display primes', function() {
+        standardFizz({s: 1, e: 100, p: true}, addToResults);
+
+        results.length.should.eql(100);
+        for (var i = 0; i < 100; i++) {
+          results[i].should.eql(oneto100p[i]);
         }
       });
     });
 
     describe('yield', function() {
       it('should run through the basic fizzbuzz test', function() {
-        yieldFizz(1, 100, addToResults);
+        yieldFizz({s: 1, e: 100}, addToResults);
 
         results.length.should.eql(100);
         for (var i = 0; i < 100; i++) {
@@ -48,14 +58,34 @@ describe('fizzbuzz', function() {
         }
       });
       it('works on numbers other than 1-100', function() {
-        yieldFizz(1, 10, addToResults);
+        yieldFizz({s: 1, e: 10}, addToResults);
 
         results.length.should.eql(10);
         for (var i = 0; i < 10; i++) {
           results[i].should.eql(oneto100[i]);
         }
       });
+      it('can also display primes', function() {
+        standardFizz({s: 1, e: 100, p: true}, addToResults);
+
+        results.length.should.eql(100);
+        for (var i = 0; i < 100; i++) {
+          results[i].should.eql(oneto100p[i]);
+        }
+      });
     });
+    
+    describe('prime', function() {
+      it('finds prime numbers', function() {
+        var isPrime = findPrime('17');
+        isPrime.should.be.true;
+      });
+      
+      it('rejects non-prime numbers', function() {
+        var isPrime = findPrime('20');
+        isPrime.should.be.false;
+      });
+    })
   });
 
   describe('integration', function() {
@@ -64,7 +94,7 @@ describe('fizzbuzz', function() {
         function (err, stdout, stderr) {
           if (err) { return done(err); }
           
-          var results = stdout.split('\n')
+          var results = stdout.split('\n');
           results.pop();
           results.should.eql(oneto100);
           stderr.should.eql('');
@@ -77,7 +107,7 @@ describe('fizzbuzz', function() {
         function (err, stdout, stderr) {
           if (err) { return done(err); }
           
-          var results = stdout.split('\n')
+          var results = stdout.split('\n');
           results.pop();
           results.should.eql(oneto100.slice(89));
           stderr.should.eql('');
@@ -90,9 +120,22 @@ describe('fizzbuzz', function() {
         function (err, stdout, stderr) {
           if (err) { return done(err); }
           
-          var results = stdout.split('\n')
+          var results = stdout.split('\n');
           results.pop();
           results.should.eql(oneto100.slice(0, 10));
+          stderr.should.eql('');
+          done();
+      });
+    });
+    
+    it('uses the prime flag', function(done) {
+      exec('./bin/fizzbuzz -p',
+        function (err, stdout, stderr) {
+          if (err) { return done(err); }
+          
+          var results = stdout.split('\n');
+          results.pop();
+          results.should.eql(oneto100p.slice(0, 100));
           stderr.should.eql('');
           done();
       });
@@ -103,7 +146,7 @@ describe('fizzbuzz', function() {
         function (err, stdout, stderr) {
           if (err) { return done(err); }
           
-          var results = stdout.split('\n')
+          var results = stdout.split('\n');
           results.pop();
           results.should.eql(oneto100.slice(9, 50));
           stderr.should.eql('');
